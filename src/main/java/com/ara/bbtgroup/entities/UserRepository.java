a51e7c96b4f2dda89defd1f10b4683135fc260d5
adnan-rasto <rastoder.adnan@hotmail.com>
2017-11-30 15:39:23 +0000

    Changed classname

--- src/main/java/com/ara/bbtgroup/entities/UserRepository.java
+++ src/main/java/com/ara/bbtgroup/entities/UserRepository.java
@@ -2,6 +2,6 @@ package com.ara.bbtgroup.entities;
 
 import org.springframework.data.repository.CrudRepository;
 
-public interface UsersRepository extends CrudRepository<User, Integer> {
+public interface UserRepository extends CrudRepository<User, Integer> {
     User findByFirstname(String firstname);
 }
--- src/test/java/com/ara/bbtgroup/BbtgroupApplicationTests.java
+++ src/test/java/com/ara/bbtgroup/BbtgroupApplicationTests.java
@@ -1,7 +1,7 @@
 package com.ara.bbtgroup;
 
 import com.ara.bbtgroup.entities.User;
-import com.ara.bbtgroup.entities.UsersRepository;
+import com.ara.bbtgroup.entities.UserRepository;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -18,19 +18,19 @@ import static org.junit.Assert.assertNull;
 public class BbtgroupApplicationTests {
 
 	@Autowired
-	private UsersRepository usersRepository;
+	private UserRepository userRepository;
 	@Before
 	public void setUp() throws Exception{
 		User user = new User("Max", "Muster");
 
 		assertNull(user.getId());
-		this.usersRepository.save(user);
+		this.userRepository.save(user);
 		assertNotNull(user.getId());
 	}
 
 	@Test
 	public void contextLoads() {
-		User userA = usersRepository.findByFirstname("Max");
+		User userA = userRepository.findByFirstname("Max");
 		assertNotNull(userA);
 
 	}
