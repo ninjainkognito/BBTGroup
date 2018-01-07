0894b8e283b859df038867520332c7ce2f37c685
adnan-rasto <rastoder.adnan@hotmail.com>
2018-01-07 20:59:04 +0000

    refactoring test classes

--- src/test/java/com/ara/bbtgroup/rest/TaskResourceTest.java
+++ src/test/java/com/ara/bbtgroup/rest/TaskResourceTest.java
@@ -2,7 +2,6 @@ package com.ara.bbtgroup.rest;
 
 import com.ara.bbtgroup.model.Task;
 import com.ara.bbtgroup.repository.TaskRepository;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,9 +12,7 @@ import java.util.Date;
 import java.util.List;
 
 import static java.lang.Long.valueOf;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -28,24 +25,12 @@ public class TaskResourceTest {
     private final Date ENDDATE = new Date(2018,01,31);
     private final Long OWNER = valueOf(1);
 
-
-    @Before
-    public void initDataIntoDatabase(){
-
-        Task task = new Task("title","description", 1, STARTDATE, ENDDATE, OWNER);
-        Task task1 = new Task("title","description", 1, STARTDATE, ENDDATE, OWNER);
-        Task task2 = new Task("title","description", 1, STARTDATE, ENDDATE, OWNER);
-
-        this.taskRepository.save(task);
-        this.taskRepository.save(task1);
-        this.taskRepository.save(task2);
-    }
-
     @Test
     public void saveTask_whenFoundById_thenOk(){
 
-        Task task = taskRepository
-                .save(new Task("title","description", 1, STARTDATE, ENDDATE, OWNER));
+        Task task = taskRepository.save(new Task("title",
+                "description", 1,
+                STARTDATE, ENDDATE, OWNER));
 
         Task foundTsk = taskRepository
                 .findOne(task.getTaskId());
@@ -57,7 +42,16 @@ public class TaskResourceTest {
     @Test
     public void saveMultipleTasks_AndFindAll_whenMultipleFound_thenOk(){
 
-        // saving multiple Tasks is automatically done with the @Before class
+        Task task = new Task("title","description",
+                1, STARTDATE, ENDDATE, OWNER);
+        Task task1 = new Task("title","description",
+                1, STARTDATE, ENDDATE, OWNER);
+        Task task2 = new Task("title","description",
+                1, STARTDATE, ENDDATE, OWNER);
+
+        this.taskRepository.save(task);
+        this.taskRepository.save(task1);
+        this.taskRepository.save(task2);
 
         List<Task> taskList = taskRepository.findAll();
 
@@ -73,17 +67,12 @@ public class TaskResourceTest {
         Task task = taskRepository
                 .save(new Task("SaveTaskAndDelete","Testing", 1, STARTDATE, ENDDATE, ownerTemp));
 
-        taskRepository.findAllByOwnerOfTheTask("4194984").size();
-
         // check if data successfully written into database
         assertTrue(taskRepository.findAllByOwnerOfTheTask("4194984").size() > 0);
 
-        int lastInsertTask = taskRepository.findAll().size();
-
-        taskRepository.delete(valueOf(lastInsertTask));
-
-        assertTrue(taskRepository.findAll().size() != lastInsertTask);
-
+        Long lastInsertTask = taskRepository.findOne(task.getTaskId()).getTaskId();
+        taskRepository.delete(lastInsertTask);
+        assertNull(taskRepository.findOne(lastInsertTask));
     }
 
     @Test
